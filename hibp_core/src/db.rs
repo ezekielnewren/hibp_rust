7ebfd98e1ca199ffadcd13af74dfa28dc8879224
Ezekiel Newren <ezekielnewren@gmail.com>
2023-12-25 04:27:56 -0700

    cache the work of creating a slice out of mmap

--- hibp_core/src/db.rs
+++ hibp_core/src/db.rs
@@ -2,10 +2,8 @@ use std::cmp::min;
 use std::ffi::c_void;
 use std::fs::File;
 use std::mem::size_of;
-use std::ops::Deref;
 use memmap2::{Mmap, MmapOptions};
 use crate::{binary_search_generate_cache, binary_search_get_range, HASH};
-// use crate::lib::{HASH, binary_search_generate_cache, binary_search_get_range};
 
 pub struct FileArray {
     pub pathname: String,
@@ -14,13 +12,14 @@ pub struct FileArray {
     mlock_result: i32,
 }
 
-pub struct HIBPDB {
+pub struct HIBPDB<'a> {
     pub index: FileArray,
+    pub index_slice: &'a [HASH],
     pub index_cache: Vec<HASH>,
 }
 
 
-impl HIBPDB {
+impl<'a> HIBPDB<'a> {
     pub fn new(v: &String, prefer_locking: bool) -> HIBPDB {
         let dbdir = v.clone();
         let mut file_index = dbdir.clone();
@@ -37,7 +36,11 @@ impl HIBPDB {
             mlock_result: 0i32,
         };
 
-        let index = unsafe { (&fa.mmap).align_to::<HASH>().1 };
+        // let index = unsafe { (&fa.mmap).align_to::<HASH>().1 };
+        let index_slice: &'a [HASH] = unsafe {
+            let ptr = fa.mmap.as_ptr() as *const HASH;
+            std::slice::from_raw_parts(ptr, fa.mmap.len()/size_of::<HASH>())
+        };
 
         if prefer_locking {
             unsafe {
@@ -48,30 +51,35 @@ impl HIBPDB {
             }
         }
 
-        let log2 = (index.len() as f64).log2().ceil() as u64;
+        let log2 = (index_slice.len() as f64).log2().ceil() as u64;
         let depth = min(log2/2, log2);
         let mut index_cache: Vec<HASH> = Vec::new();
-        binary_search_generate_cache(&index, 0..index.len() as u64, &mut index_cache, depth);
+        binary_search_generate_cache(&index_slice, 0..index_slice.len() as u64, &mut index_cache, depth);
 
         Self {
             index: fa,
+            index_slice,
             index_cache,
         }
     }
 
     pub fn index(self: &Self) -> &[HASH] {
-        return unsafe { self.index.mmap.align_to::<HASH>().1 };
+        return self.index_slice;
     }
 
     pub fn find(self: &mut Self, key: &HASH) -> Result<usize, usize> {
-        let mut range = 0..self.index().len() as u64;
-        range = binary_search_get_range(&self.index_cache, &range, &key);
-        return self.index().binary_search(&key);
+        return if self.index.mlock_result == 0 {
+            self.index().binary_search(&key)
+        } else {
+            let mut range = 0..self.index().len() as u64;
+            range = binary_search_get_range(&self.index_cache, &range, &key);
+            self.index().binary_search(&key)
+        }
     }
 }
 
 
-impl Drop for HIBPDB {
+impl Drop for HIBPDB<'_> {
     fn drop(&mut self) {
         if self.index.mlock_result == 0 {
             unsafe {

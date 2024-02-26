7e118e4cab2b66887df47221dd016720f9b66716
Ezekiel Newren <ezekielnewren@gmail.com>
2024-02-26 08:29:58 -0700

    implement binary search for IndexByCopy

--- hibp_core/src/indexbycopy.rs
+++ hibp_core/src/indexbycopy.rs
@@ -1,4 +1,4 @@
-
+use std::cmp::Ordering;
 
 pub trait IndexByCopy<T: Copy> {
     fn get(&mut self, index: usize) -> T;
@@ -7,5 +7,32 @@ pub trait IndexByCopy<T: Copy> {
 
 pub trait IndexByCopyMut<T: Copy> {
     fn set(&mut self, index: usize, value: T);
+}
+
+pub fn binary_search_by<T: Ord + Copy, F>(arr: &mut dyn IndexByCopy<T>, mut f: F) -> Result<usize, usize>
+    where
+        F: FnMut(T) -> Ordering,
+{
+    let mut lo = 0;
+    let mut hi = arr.len()-1;
+
+    while lo <= hi {
+        let mid = (lo+hi)>>1;
+        let cmp = f(arr.get(mid));
+
+        if cmp.is_eq() {
+            return Ok(mid);
+        } else if cmp.is_lt() {
+            lo = mid+1;
+        } else {
+            hi = mid-1;
+        }
+    }
+
+    Err(lo)
+}
+
+pub fn binary_search<T: Copy + Ord>(arr: &mut dyn IndexByCopy<T>, key: T) -> Result<usize, usize> {
+    return binary_search_by(arr, |p| p.cmp(&key));
 }
 
